86fd46e66b33f9cfac3e9c2536db33ad8ca8515a
aberhel <38218977+aberhel@users.noreply.github.com>
2018-04-21 18:47:14 -0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = ACe032167d32e402718ace34729e2b1402
+auth_token = 2e55b866cc5cba20e29050f167e6ae95
+my_cell = +17075900590
+my_twilio = +19252394204
