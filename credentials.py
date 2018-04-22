4073378871b70f7020fac60552c16ad84bfee7dc
aberhel <38218977+aberhel@users.noreply.github.com>
2018-04-21 18:50:23 -0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = ACe032167d32e402718ace34729e2b1402
-auth_token = 2e55b866cc5cba20e29050f167e6ae95
-my_cell = +17075900590
-my_twilio = +19252394204
+account_sid = 'ACe032167d32e402718ace34729e2b1402'
+auth_token = '2e55b866cc5cba20e29050f167e6ae95'
+my_cell = '+17075900590'
+my_twilio = '+19252394204'
